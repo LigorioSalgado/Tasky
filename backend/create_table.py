a49284f61d8cbe88775ea87d92f40741ce5e803b
edwinsalgado10 <edwin.salgado.con@thrivemarket.com>
2024-08-02 16:27:48 -0600

    adding new create table

--- backend/create_table.py
+++ backend/create_table.py
@@ -8,30 +8,7 @@ def create_tables():
             write_capacity_units=1, 
             wait=True
         )
-        print("Tabla 'Proyectos' creada exitosamente.")
-    else:
-        print("La tabla 'Proyectos' ya existe.")
-
-    if not Column.exists():
-        Column.create_table(
-            read_capacity_units=1, 
-            write_capacity_units=1, 
-            wait=True
-        )
-        print("Tabla 'Columnas' creada exitosamente.")
-    else:
-        print("La tabla 'Columnas' ya existe.")
-
-    if not Task.exists():
-        Task.create_table(
-            read_capacity_units=1, 
-            write_capacity_units=1, 
-            wait=True
-        )
-        print("Tabla 'Tareas' creada exitosamente.")
-    else:
-        print("La tabla 'Tareas' ya existe.")
-
+        print("Table Projects was created")
 
 if __name__ == "__main__":
     create_tables()
